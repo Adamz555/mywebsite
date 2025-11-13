49b01a6f46f0b42cec28cdf24dd2a6d865c735eb
Ajmal Adam <you@example.com>
2025-11-13 10:29:38 +0530

    Updated website content & profile image

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, send_from_directory
+app = Flask(__name__, static_folder='static', template_folder='templates')
+
+@app.route('/')
+def index():
+    return render_template('index.html', title="AJMAL ADAM Blockchain Research & Technologies")
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title="About | Ajmal Adam")
+
+@app.route('/research')
+def research():
+    return render_template('research.html', title="Research | Ajmal Adam")
+
+@app.route('/knowledge')
+def knowledge():
+    return render_template('knowledge.html', title="History of Blockchain | Ajmal Adam")
+
+@app.route('/blockchain-basic')
+def blockchain_basic():
+    return render_template('blockchain_basic.html', title="Blockchain Basic | Ajmal Adam")
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title="Contact | Ajmal Adam")
+
+@app.route('/labs')
+def labs():
+    return render_template('labs.html', title="🧪 Labs | Ajmal Adam Research")
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080)
